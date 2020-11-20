4d5acc5bdb280ca1436fd501f6a6f76518cce7e1
Sudarshan Guttula <sidguttula@yahoo.com>
2020-11-20 14:49:31 -0800

    Send and receive data connection

---
+++ SocketCubeToSim/SimplatServer.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket()
+port = 12345
+s.bind(('', port))
+s.listen(5)
+c, addr = s.accept()
+print ("Socket Up and running with a connection from",addr)
+while True:
+    rcvdData = c.recv(1024).decode()
+    print ("S:",rcvdData)
+    sendData = rcvdData
+
+    # sendData = rcvdData
+    c.send(sendData.encode())
+    if(sendData == "Bye" or sendData == "bye"):
+        break
+c.close()
